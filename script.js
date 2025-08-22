06cb73f4f299da5973153b72f9a2abecb8fff273
Vadim Davidov <vdvdavid21@gmail.com>
2025-08-22 13:13:20 +0300

    leaflet map added

--- script.js
+++ script.js
@@ -31,3 +31,14 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' slider-active';
 }
+
+// * Leaflet map
+
+const map = L.map('map').setView([44.769459, 17.181912], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+let marker = L.marker([44.769459, 17.181912]).addTo(map);
